55e8ddf05613d7a23a1afe652681734366553d97
Nazreen <nxmohamad@gmail.com>
2019-11-19 00:31:04 +0000

    setup server

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const server = require('./server.js');
+
+const init = async () => {
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
