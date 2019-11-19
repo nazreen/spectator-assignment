a30d54a7a2b3d49e1f1f82c1b54bb1d4547ed703
Nazreen <nxmohamad@gmail.com>
2019-11-19 23:30:16 +0000

    register route in entrypoint

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
-const server = require('./server.js');
+const server = require('./server.js')
+const calculateBasketRoutes = require('./calculate-basket/routes')
+
+const routes = [calculateBasketRoutes]
 
 const init = async () => {
+  await server.register(routes)
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
 
-  await server.start();
-  console.log('Server running on %s', server.info.uri);
-};
+process.on('unhandledRejection', err => {
+  console.log(err)
+  process.exit(1)
+})
 
-process.on('unhandledRejection', (err) => {
-
-  console.log(err);
-  process.exit(1);
-});
-
-init();
+init()
