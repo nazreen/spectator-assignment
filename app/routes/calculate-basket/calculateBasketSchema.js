bab3eb47507813d977cd22599a9544ba9bc9f9b1
Nazreen <nxmohamad@gmail.com>
2019-11-20 06:00:08 +0000

    whitelist currency

--- app/routes/calculate-basket/calculateBasketSchema.js
+++ app/routes/calculate-basket/calculateBasketSchema.js
@@ -14,9 +14,15 @@ const schema = Joi.object({
   items: customJoi
     .stringArray()
     .min(1)
+    .items(
+      Joi.string()
+        .only()
+        .allow('Apples', 'Milk', 'Soup', 'Bread')
+    )
     .prefs({ convert: true })
     .required(), // TODO allow only whitelisted
   currency: Joi.string()
+    .only()
     .allow('GBP', 'USD', 'EUR')
     .required()
 })
