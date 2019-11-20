6c4b435ed3ddf80bb37bffca2763bf0138395c74
Nazreen <nxmohamad@gmail.com>
2019-11-20 01:05:32 +0000

    update name

--- app/constructors/index.spec.js
+++ app/constructors/index.spec.js
@@ -1,29 +1,50 @@
-const { BasketCost, Subtotal } = require('./index')
+const { BasketCost, ItemsCount, Subtotal } = require('./index')
 const priceList = require('../data/priceList')
 
-describe('BasketCost', () => {
-  it('given items, generates list accurately', () => {
+fdescribe('ItemsCount', () => {
+  fit('given items, generates list accurately', () => {
     const items = ['Apples', 'Apples', 'Milk']
-    const result = new BasketCost(items, 'USD').itemsCount
+    const result = new ItemsCount(items).dict
     expect(result).toEqual({ Apples: 2, Milk: 1 })
   })
 
-  it('given items, generates list accurately', () => {
+  fit('given items, generates list accurately', () => {
     const items = ['Apples', 'Apples', 'Milk', 'Bread']
-    const result = new BasketCost(items, 'USD').itemsCount
+    const result = new ItemsCount(items, 'USD').dict
     expect(result).toEqual({ Apples: 2, Milk: 1, Bread: 1 })
   })
 })
 
-fdescribe('Subtotal: ', () => {
+describe('Subtotal: ', () => {
   fit('calculates : Apples,Apples', () => {
     const itemsCount = { Apples: 2 }
     const result = new Subtotal(itemsCount, priceList['USD']).subtotal
     expect(result).toEqual(2.0)
   })
-  fit('calculates correctly: Apples, Milk', () => {
+  it('calculates correctly: Apples, Milk', () => {
     const itemsCount = { Apples: 1, Milk: 1 }
     const result = new Subtotal(itemsCount, priceList['USD']).subtotal
     expect(result).toEqual(2.15)
+  })
+})
+
+fdescribe('DiscountCalculation: ', () => {
+  fit('no discount: bread only', () => {
+    const itemsCount = { Bread: 1 }
+    const result = new DiscountCalculation(itemsCount, activeDiscounts)
+    expect(result.discounts).toEqual([])
+    expect(result.discountAmt).toEqual(0)
+  })
+  it('apple discount', () => {
+    const itemsCount = { Apples: 2, Milk: 1 }
+    const result = new DiscountCalculation(itemsCount, activeDiscounts)
+    expect(result.discounts).toEqual(['Apples 10% discount'])
+    expect(result.discountAmt).toEqual(0.2)
+  })
+  it('milk discount', () => {
+    const itemsCount = { Bread: 2, Milk: 3 }
+    const result = new DiscountCalculation(itemsCount, activeDiscounts)
+    expect(result.discounts).toEqual(['50 cent discount when buying 3 Milks'])
+    expect(result.discountAmt).toEqual(0.5)
   })
 })
--- app/routes/calculate-basket/handleCalculateBasket.js
+++ app/routes/calculate-basket/handleCalculateBasket.js
@@ -2,6 +2,7 @@ const { BasketCost, ItemsCount, Subtotal } = require('../../constructors/index')
 
 function handleCalculateBasket(request, h) {
   const { items, currency } = request.url.searchParams
+  console.log('here', { items, currency })
   const { output } = new BasketCost(items, currency)
   return output
 }
